4813dad1de75f419292d6ba804ef453943cbaa2e
Sean LaPlante <laplantes@anfosec.com>
2024-08-30 15:11:05 -0400

    Removed AIS-specific/gitlab-related files and scripts

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-. ./scripts/build_deps.sh
+DATE=$(git log -1 --format=%cd --date=format:%Y%m%d.%H%M%S)
 
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DDOXYGEN=1 ..
-make -j4 package
+cmake -DCMAKE_BUILD_TYPE=Release -DDOXYGEN=1 -DPACKAGE_PATCH_VERSION_EXTRA=$DATE ..
+make -j package
 
--- scripts/update_ci_repo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Update the repository
-sshpass -e ssh gitlab-runner@build.ais -C "/usr/local/bin/mutex sudo -H /usr/bin/freight-cache -c /etc/freight.build.ais.conf"
