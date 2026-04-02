9fedba5b50afd01adbf94d7a8fbbc1072d6ef4bf
Sean LaPlante <laplante.sean@gmail.com>
2026-04-02 19:33:17 -0400

    Update ivfilemon python to support ignore processes, process tracking and better JSON output

--- pyintrovirt/pyintrovirt/tools/ivfilemon.py
+++ pyintrovirt/pyintrovirt/tools/ivfilemon.py
@@ -11,11 +11,14 @@ Usage:
 import sys
 import json
 import argparse
+import threading
 import functools
 import traceback
 from pyintrovirt import VMI, EventType, Event, OS, SystemCallIndex, WindowsSystemCall
 
 HANDLE_TRACKER: set[tuple[int, int]] = set()
+PROCESS_TRACKER: set[int] = set()
+TRACKER_LOCK = threading.Lock()
 
 
 def normalize_path(path: str) -> str:
@@ -57,25 +60,68 @@ def get_file_handle(handler) -> int:
     return handler.FileHandle()
 
 
-def emit(event: Event, *, print_json: bool, pretty_json: bool):
+def normalize_dict(event: Event, event_source: str) -> dict:
+    """Normalize the event dictionary."""
+    obj = event.to_dict().get("event", {})
+    result = {
+        "source": event_source,
+        "arguments": obj.get("syscall", {}).get("arguments", {}),
+        "index": obj.get("syscall", {}).get("index", None),
+        "result": obj.get("syscall", {}).get("result", {}),
+        "pid": obj.get("task", {}).get("pid", None),
+        "tid": obj.get("task", {}).get("tid", None),
+        "process_name": obj.get("task", {}).get("process_name", None)
+    }
+    # Flatten the output JSON so it's not so nested
+    return result
+
+
+def emit_process_event(event: Event, *, print_json: bool, pretty_json: bool):
     """Log the event."""
     if print_json:
-        sys.stdout.write(json.dumps(event.to_dict(), indent=2 if pretty_json else None) + "\n")
+        obj = normalize_dict(event, "process_tracking")
+        sys.stdout.write(json.dumps(obj, indent=2 if pretty_json else None) + "\n")
     else:
-        sys.stdout.write(str(event) + "\n")
+        sys.stdout.write(f"PROCESS TRACKING: {str(event)}\n")
 
 
-def handle_sysret(_vmi: VMI, event: Event, *, monitor_file_paths: list[str], print_json: bool, pretty_json: bool):
+def emit_file_event(event: Event, *, print_json: bool, pretty_json: bool):
+    """Log the event."""
+    if print_json:
+        obj = normalize_dict(event, "file_handle_tracking")
+        sys.stdout.write(json.dumps(obj, indent=2 if pretty_json else None) + "\n")
+    else:
+        sys.stdout.write(f"FILE HANDLE TRACKING: {str(event)}\n")
+
+
+def handle_sysret(_vmi: VMI, event: Event, *, monitor_file_paths: list[str], print_json: bool, pretty_json: bool, follow_process: bool, ignore_processes: set[str]):
     """Handle system call return."""
     handler: WindowsSystemCall = event.get_syscall_handler()
+
+    match event.syscall_index:
+        case SystemCallIndex.NtTerminateProcess:
+            if follow_process and handler.result().NT_SUCCESS():
+                target = handler.target_pid()
+                with TRACKER_LOCK:
+                    if target in PROCESS_TRACKER:
+                        emit_process_event(event, print_json=print_json, pretty_json=pretty_json)
+                        PROCESS_TRACKER.discard(target)
+                        return
+
+    if follow_process:
+        with TRACKER_LOCK:
+            if event.pid in PROCESS_TRACKER:
+                emit_process_event(event, print_json=print_json, pretty_json=pretty_json)
+
     handle: int = get_file_handle(handler)
     if handle == 0:
         return  # Nothing to do
 
     key = (event.pid, handle)
-    if key in HANDLE_TRACKER:
-        emit(event, print_json=print_json, pretty_json=pretty_json)
-        return
+    with TRACKER_LOCK:
+        if key in HANDLE_TRACKER:
+            emit_file_event(event, print_json=print_json, pretty_json=pretty_json)
+            return
 
     obj_attr = get_object_attributes(handler)
     if not obj_attr:
@@ -83,13 +129,19 @@ def handle_sysret(_vmi: VMI, event: Event, *, monitor_file_paths: list[str], pri
 
     for path in monitor_file_paths:
         if path_matches(path, obj_attr, event.kpcr):
-            HANDLE_TRACKER.add(key)
-            emit(event, print_json=print_json, pretty_json=pretty_json)
+            with TRACKER_LOCK:
+                HANDLE_TRACKER.add(key)
+                if follow_process:
+                    PROCESS_TRACKER.add(event.pid)
+            emit_file_event(event, print_json=print_json, pretty_json=pretty_json)
             return
 
 
-def handle_syscall(_vmi: VMI, event: Event, *, print_json: bool, pretty_json: bool):
+def handle_syscall(_vmi: VMI, event: Event, *, print_json: bool, pretty_json: bool, follow_process: bool, ignore_processes: set[str]):
     """Handle a system call."""
+    if event.process_name.strip().lower() in ignore_processes:
+        return
+
     if event.will_return():
         event.hook_return(True)
 
@@ -98,8 +150,27 @@ def handle_syscall(_vmi: VMI, event: Event, *, print_json: bool, pretty_json: bo
     match event.syscall_index:
         case SystemCallIndex.NtClose:
             key = (event.pid, get_file_handle(handler))
-            HANDLE_TRACKER.discard(key)
-            emit(event, print_json=print_json, pretty_json=pretty_json)
+            with TRACKER_LOCK:
+                if key in HANDLE_TRACKER:
+                    HANDLE_TRACKER.discard(key)
+                    emit_file_event(event, print_json=print_json, pretty_json=pretty_json)
+        case SystemCallIndex.NtTerminateProcess:
+            if not follow_process:
+                return
+            target = handler.target_pid()
+            with TRACKER_LOCK:
+                if target in PROCESS_TRACKER and (not event.will_return() or target == event.pid):
+                    emit_process_event(event, print_json=print_json, pretty_json=pretty_json)
+                    PROCESS_TRACKER.discard(target)
+                    return
+        case _:
+            if not follow_process:
+                return
+            if not event.will_return():
+                with TRACKER_LOCK:
+                    if event.pid in PROCESS_TRACKER:
+                        emit_process_event(event, print_json=print_json, pretty_json=pretty_json)
+
 
 
 def main():
@@ -115,16 +186,22 @@ def main():
         dest="file_paths",
         required=True,
     )
+    parser.add_argument("--follow-process", action="store_true", help="Follow the processes that access monitored files")
+    parser.add_argument("--ignore-process", action="append", help="Ignore this process for file access and process tracking (can be specified multiple times)", dest="ignore_processes")
     parser.add_argument("--json", action="store_true", help="Print the events as JSON")
     parser.add_argument("--pretty-json", action="store_true", help="Pretty-print JSON output (Warning: LOUD)")
     args = parser.parse_args()
 
     print_json = bool(args.json)
     pretty_json = bool(args.pretty_json)
+    follow_process = bool(args.follow_process)
 
     monitor_file_paths: list[str] = []
     for path in args.file_paths:
         monitor_file_paths.append(normalize_path(path))
+    ignore_processes: set[str] = set()
+    for process in args.ignore_processes:
+        ignore_processes.add(process.strip().lower())
     rc = 1
 
     try:
@@ -133,15 +210,27 @@ def main():
                 print("Only Windows guests are supported for this tool.")
                 return rc
 
-            print(f"Guest: {vmi.guest_os().name}")
+            print(f"Guest: {vmi.guest_os().name}", file=sys.stderr)
 
-            vmi.filter_system_call_category("file")
-            syscall_handler = functools.partial(handle_syscall, print_json=print_json, pretty_json=pretty_json)
+            if not follow_process:
+                vmi.filter_system_call_category("file")
+            else:
+                vmi.default_system_call_filter()
+
+            syscall_handler = functools.partial(
+                handle_syscall,
+                print_json=print_json,
+                pretty_json=pretty_json,
+                follow_process=follow_process,
+                ignore_processes=ignore_processes
+            )
             sysret_handler = functools.partial(
                 handle_sysret,
                 monitor_file_paths=monitor_file_paths,
                 print_json=print_json,
                 pretty_json=pretty_json,
+                follow_process=follow_process,
+                ignore_processes=ignore_processes
             )
             vmi.register_callback(EventType.EVENT_FAST_SYSCALL, syscall_handler)
             vmi.register_callback(EventType.EVENT_FAST_SYSCALL_RET, sysret_handler)
